c8466114094a40060f2cab72dffb1fa636174ec4
Bill Raper <bill.raper@g2o.com>
2023-11-08 17:30:45 +0000

    yup

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,11 @@
+    select
+        id,
+        orderid as order_id,
+        paymentmethod,
+        status,
+        amount/100 as amount,
+        created,
+        _batched_at
+
+    from {{ source('stripe', 'payment') }}
+
